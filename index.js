04c316b72e78fc599bf3df8c31c06d3485bfc7fa
Sergio <kurisergio777777@gmail.com>
2022-04-04 22:48:33 -0400

    commit tree 60bbeee80748834b11b6c886a97a8efecd820d4d

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // code your solution here
+function saturdayFun(planA = "roller-skate"){
+return(`This Saturday, I want to ${planA}!`)
+};
+
+
+
+
+function mondayWork( planA = "go to the office"){
+        return(`This Monday, I will ${planA}.`)
+
+    }
+
+
+function wrapAdjective(name = "*") {
+        return function (single = "special") {
+          return `You are ${name}${single}${name}!`;
+        };
+      }
+
+      
+    
+
+
+
+
+
+
+
+
+/*
+
+function wrapAdjective(name = "*")  {
+    const innerFunction = function (parameter = "special") {
+        return innerFunction(`<You are ${parameter}`)
+      };
+      return wrapAdjective(`<You are ${name}`)
+    };
+  
+  wrapAdjective ("Sergio")("you");
+
+  */
